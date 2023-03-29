428aaddd015283faa5d7b1dc3300a85a4af57323
Josh Nakka <joshnakka1@gmail.com>
2023-03-29 08:21:15 -0400

    Added models route for future use

---
+++ src/pages/api/models.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { OpenAIApi, Configuration } from "openai";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const apiKey = (req.headers["authorization"] as string)?.split(" ")[1];
+  if (!apiKey) {
+    return res.status(401).json({ error: "Missing token" });
+  }
+
+  const configuration = new Configuration({
+    apiKey,
+  })
+
+  const openAi = new OpenAIApi(configuration);
+
+  const { data } = await openAi.listModels();
+
+  return res.status(200).json(data);
+}
