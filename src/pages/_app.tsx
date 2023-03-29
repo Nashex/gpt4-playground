34048432fa32a809cb4a2db6f91f68652b84127e
Josh Nakka <joshnakka1@gmail.com>
2023-03-29 14:30:46 -0400

    Added Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from "@/context/AuthProvider";
 import OpenAIProvider from "@/context/OpenAIProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   if (typeof window !== "undefined") {
@@ -19,10 +20,13 @@ export default function App({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <AuthProvider>
-      <OpenAIProvider>
-        <Component {...pageProps} />
-      </OpenAIProvider>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <OpenAIProvider>
+          <Component {...pageProps} />
+        </OpenAIProvider>
+      </AuthProvider>
+      <Analytics />
+    </>
   );
 }
